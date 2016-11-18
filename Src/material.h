1426d168d12eb9d4ab4fff08e8b2f1bc903a2287
miyawaki-toru <asuruntoru0803@yahoo.co.jp>
2016-11-18 13:55:56 +0900

    マテリアルクラスとシェーディング情報クラス追加

--- Src/material.h
+++ Src/material.h
@@ -5,4 +5,14 @@
 
 typedef Vec Color;
 
+struct Material
+{
+	Material()
+	{
+		albedo = Color(1, 1, 1);
+	}
+
+	Color albedo;	//!< ”½ŽË—¦
+};
+
 #endif // !_MATERIAL_H_
---
+++ Src/shade_record.h
@@ -0,0 +1,22 @@
+#ifndef _SHADE_RECORD_H_
+#define	_SHADE_RECORD_H_
+
+#include "material.h"
+
+/**
+* @beief シェーディング情報を詰めた構造体
+*/
+struct ShadeRecord
+{
+	ShadeRecord()
+	{
+		is_intersect = false;
+	}
+
+	bool		is_intersect;	//!< レイと交差したか
+	Material	material;		//!< マテリアル
+	Vec			normal;			//!< 法線
+	Vec			hit_point;		//!< 交差点
+};
+
+#endif // !_SHADE_RECORD_H_
