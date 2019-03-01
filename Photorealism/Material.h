7e05014d41775f22117a0e221bb2b0c1018fbdfb
miyawaki-toru <asuruntoru0803@yahoo.co.jp>
2019-03-01 14:29:04 +0900

    Photorealism →ガラスマテリアル追加（バグあり）

--- Photorealism/Material.h
+++ Photorealism/Material.h
@@ -5,18 +5,49 @@
 #include "Vec3.h"
 #include "Random.h"
 
-/// 反射ベクトル
-Vec3 Reflect(const Vec3& v, const Vec3& n)
-{
-    return -v + 2.0 * Dot(v, n) * n;
-}
-
 /// ローカル座標系のベクトルからcosThetaを計算
 double CosTheta(const Vec3& v)
 {
     return v.y;
 }
 
+/// ローカル座標系のベクトルからcosThetaを計算(絶対値)
+double AbsCosTheta(const Vec3& v)
+{
+    return std::abs(v.y);
+}
+
+/// 反射ベクトル
+Vec3 Reflect(const Vec3& v, const Vec3& n)
+{
+    return -v + 2.0 * Dot(v, n) * n;
+}
+
+/// フレネル反射率
+double fresnel(const Vec3& v, const Vec3& n, double n1, double n2)
+{
+    double f0 = std::pow((n1 - n2) / (n1 + n2), 2.0);
+    double cos = AbsCosTheta(v);
+    return f0 + (1.0 - f0) * std::pow(1.0 - cos, 5.0);
+}
+
+/// 屈折ベクトル
+bool Refract(const Vec3& v, Vec3& r, const Vec3& n, double n1, double n2)
+{
+    double cos = AbsCosTheta(v);
+    double sin = std::sqrt(std::max(1.0 - cos * cos, 0.0));
+    double alpha = n1 / n2 * sin;
+
+    // 全反射
+    if (alpha * alpha > 1.0) { return false; }
+
+    // 屈折
+    r = n1 / n2 * (-v + Dot(v, n) * n) - std::sqrt(1.0 - alpha * alpha) * n;
+
+    return true;
+}
+
+/// マテリアル
 class Material
 {
 public:
@@ -24,6 +55,7 @@ public:
     virtual Vec3 Sample(const Vec3& wo, Vec3& wi, double& pdf) const = 0;
 };
 
+/// ディフューズ
 class Diffuse : public Material
 {
 public:
@@ -57,6 +89,7 @@ public:
     }
 };
 
+/// ミラー
 class Mirror : public Material
 {
 public:
@@ -71,6 +104,63 @@ public:
         pdf = 1.0;
 
         return 1.0 / CosTheta(wi) * Vec3(1);
+    }
+};
+
+/// ガラス
+class Glass : public Material
+{
+public:
+    double ior; // 屈折率
+
+    Glass(double ior) : ior(ior) {}
+
+    Vec3 Sample(const Vec3& wo, Vec3& wi, double& pdf) const override
+    {
+        // カラスに入射しているか
+        bool isEntering = CosTheta(wo) > 0.0;
+
+        // 入射と出射で屈折率、法線を入れ替え
+        double n1;  // 入射側屈折率
+        double n2;  // 出射側屈折率
+        Vec3 normal;
+        if (isEntering)
+        {
+            n1 = 1.0;
+            n2 = ior;
+            normal = Vec3(0, 1, 0);
+        }
+        else
+        {
+            n1 = ior;
+            n2 = 0.0;
+            normal = Vec3(0, -1, 0);
+        }
+
+        // フレネル反射率
+        double fr = fresnel(wo, normal, n1, n2);
+
+        // 反射
+        if (Rnd() < fr)
+        {
+            wi = Reflect(wo, normal);
+            pdf = fr;
+            return fr / AbsCosTheta(wi) * Vec3(1);
+        }
+        else // 屈折
+        {
+            if (Refract(wo, wi, normal, n1, n2))
+            {
+                pdf = 1.0 - fr;
+                return std::pow(n1/ n2, 2.0) * (1.0 - fr) / AbsCosTheta(wi) * Vec3(1);
+            }
+            else // 全反射
+            {
+                wi = Reflect(wo, normal);
+                pdf = 1.0 - fr;
+                return (1.0 - fr) / AbsCosTheta(wi) * Vec3(1);
+            }
+        }
     }
 };
 
--- Photorealism/Ray.h
+++ Photorealism/Ray.h
@@ -9,6 +9,7 @@ class Ray
 public:
     Vec3 origin;
     Vec3 direction;
+    double tmin = 0.001;
 
     Ray(const Vec3& origin, const Vec3& direction) : origin(origin), direction(direction) {}
 
